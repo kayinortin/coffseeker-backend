586eb9559dbe36bb4f4e222e7a178914e9a9c9e4
Edison <kayinortin@hotmail.com>
2023-10-03 08:37:48 +0800

    改為四筆熱門商品

--- routes/popular-products.js
+++ routes/popular-products.js
@@ -4,7 +4,7 @@ const router = express.Router()
 import { getProducts, getProductsWithQS } from '../models/products.js'
 
 router.get('/', async (req, res, next) => {
-  const products = await getProductsWithQS('', { popularity: 'desc' }, 8)
+  const products = await getProductsWithQS('', { popularity: 'desc' }, 4)
   res.json({ products })
 })
 
