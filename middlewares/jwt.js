7bc42096c9b865c2cbab07734173a5d35a1f9aa5
Edison <kayinortin@hotmail.com>
2023-10-04 16:18:28 +0800

    變更成401狀態

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -5,24 +5,18 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
 
 export default function authenticate(req, res, next) {
   const token = req.cookies.accessToken
+  console.log('token', token)
 
   if (!token) {
-    return res.json({ message: 'Forbidden', code: '403' })
-  }
-
-  if (token) {
-    jsonwebtoken.verify(token, accessTokenSecret, (err, user) => {
-      if (err) {
-        console.error(err)
-        return res.json({ message: 'Forbidden', code: '403' })
-      }
-
-      // 將user資料加到req中
-      // postman header authorization Bearer token
-      req.user = user
-      next()
-    })
-  } else {
     return res.json({ message: 'Unauthorized', code: '401' })
   }
+
+  jsonwebtoken.verify(token, accessTokenSecret, (err, user) => {
+    if (err) {
+      console.error(err)
+      return res.json({ message: 'Forbidden', code: '403' })
+    }
+    req.user = user
+    next()
+  })
 }
