422d2be58b056aaa4ac0cd8a55ea6b7a8c053cda
Edison <kayinortin@hotmail.com>
2023-10-11 01:44:41 +0800

    多值搜尋

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,6 @@
 import express from 'express'
 const router = express.Router()
 
-import { readJsonFile } from '../utils/json-tool.js'
-
 import {
   getProducts,
   getProductsWithQS,
@@ -10,42 +8,45 @@ import {
   countWithQS,
   updateProduct,
 } from '../models/products.js'
-// 專用處理sql字串的工具，主要format與escape，防止sql injection
 import sqlString from 'sqlstring'
 
-// 獲得所有資料，加入分頁與搜尋字串功能，單一資料表處理
-// products/qs?page=1&keyword=xxxx&cat_ids=1,2&sizes=1,2&tags=3,4&colors=1,2,3&orderby=id,asc&perpage=10&price_range=1500,10000
+function handleMultipleValues(field, values) {
+  if (values) {
+    const valuesArray = values.split(',')
+    return `${field} IN (${valuesArray
+      .map((v) => sqlString.escape('%' + v + '%'))
+      .join(',')})`
+  }
+  return ''
+}
+
 router.get('/qs', async (req, res, next) => {
-  // 獲取網頁的搜尋字串
   const {
     page,
     keyword,
-    product_ids,
-    colors,
+    description,
+    origin,
     orderby,
     perpage,
     price_range,
     search,
+    Roast_degree,
+    Processing,
+    Variety,
   } = req.query
 
-  // 建立資料庫搜尋條件
   const conditions = []
 
-  // 關鍵字 keyword 使用 `name LIKE '%keyword%'`
   conditions[0] = keyword
     ? `name LIKE ${sqlString.escape('%' + keyword + '%')}`
     : ''
+  conditions[1] = handleMultipleValues('description', description)
+  conditions[2] = handleMultipleValues('origin', origin)
+  conditions[3] = handleMultipleValues('Roast_degree', Roast_degree)
+  conditions[4] = handleMultipleValues('Processing', Processing)
+  conditions[5] = handleMultipleValues('Variety', Variety)
 
-  // 分類，product_id 使用 `product_id IN (1, 2, 3, 4, 5)`
-  conditions[1] = product_ids ? `product_id IN (${product_ids})` : ''
-
-  // 顏色: FIND_IN_SET(1, color) OR FIND_IN_SET(2, color)
-  const color_ids = colors ? colors.split(',') : []
-  conditions[2] = color_ids
-    .map((v) => `FIND_IN_SET(${Number(v)}, color)`)
-    .join(' OR ')
-
-  //模糊搜尋商品名稱(name)與敘述(description)
+  // 模糊搜尋商品名稱(name)與敘述(description)
   // 處理模糊搜尋字串的函式，預設搜尋字串用逗號分隔，例如"api/products/qs?search=中焙,花香"，會顯示所有商品名稱以及敘述有中焙或花香的商品
   function createSearchConditions(searchString) {
     const searchKeywordsArr = searchString.split(',')
@@ -65,14 +66,11 @@ router.get('/qs', async (req, res, next) => {
   const min = Number(priceRanges[0])
   const max = Number(priceRanges[1])
   // 價格要介於1500~10000間
-  if (min >= 1500 && max <= 10000) {
-    conditions[5] = `price BETWEEN ${min} AND ${max}`
+  if (min >= 100 && max <= 10000) {
+    conditions[5] = `discountPrice BETWEEN ${min} AND ${max}`
   }
 
-  //各條件為AND相接(不存在時不加入where從句中)
   const conditionsValues = conditions.filter((v) => v)
-
-  // 各條件需要先包含在`()`中，因各自內查詢是OR, 與其它的是AND
   const where =
     conditionsValues.length > 0
       ? `WHERE ` + conditionsValues.map((v) => `( ${v} )`).join(' AND ')
